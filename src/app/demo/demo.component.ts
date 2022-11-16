6f06de3b7d1ef4f661ee240e8c1b05997612d6fc
catherinekimani <catherinekimani882@gmail.com>
2022-11-16 11:35:28 +0300

    Add input decorator

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input ,OnChanges, SimpleChanges, DoCheck} from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class DemoComponent implements OnInit,OnChanges,DoCheck{
+  @Input() value: string = "Life Cycle Hooks"
+  constructor() { 
+    console.log('constructor');
+    // console.log(this.value);
   }
-
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges called")
+    console.log(changes)
+  }
+  ngOnInit(): void {
+    console.log("ngOnInit called!");
+    // console.log(this.value);
+  }
+ngDoCheck(){}
 }
